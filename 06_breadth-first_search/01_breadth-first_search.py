90ec44ba8c712a1337f5800a8a8024625af4a42f
YinGuiqing <CPLASFyin@163.com>
2019-06-08 15:34:31 +0800

    2019-06-08 完成程序

--- 06_breadth-first_search/01_breadth-first_search.py
+++ 06_breadth-first_search/01_breadth-first_search.py
@@ -2,6 +2,8 @@
 # 广度优先搜索
 # 2019-06-08
 
+from collections import deque
+
 graph = {}
 graph["you"] = ["alice", "bob", "claire"]
 graph["bob"] = ["anuj", "peggy"]
@@ -12,4 +14,22 @@ graph["peggy"] = []
 graph["thom"] = []
 graph["jonny"] = []
 
-print(graph)
+def search(name):
+    search_queue = deque()
+    search_queue += graph[name]
+    searched = []       # 用于记录检查过的人
+    while search_queue:
+        person = search_queue.popleft()
+        if not person in searched:      # 仅当这个人没检查过时才检查
+            if person_is_seller(person):
+                print(person + " is a mango seller!")
+                return True
+            else:
+                search_queue += graph[person]
+                searched.append(person)     # 将这个人标记为检查过
+    return False
+
+def person_is_seller(name):
+    return name[-1] == 'm'
+
+search("you")
