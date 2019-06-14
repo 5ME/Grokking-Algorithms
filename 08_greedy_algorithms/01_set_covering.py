eef8c3fc977190b7c14ee5b00e4955cafda92768
YinGuiqing <CPLASFyin@163.com>
2019-06-14 18:02:22 +0800

    2019-06-14 完成了第8章前3节的学习

---
+++ 08_greedy_algorithms/01_set_covering.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# 贪婪算法
+# 2019-06-14
+
+# 要覆盖的州
+states_needed = set(["mt", "wa", "or", "id", "nv", "ut", "ca", "az"])
+
+# 可供选择的广播台清单
+stations = {}
+stations["kone"] = set(["id", "nv", "ut"])
+stations["ktwo"] = set(["wa", "id", "mt"])
+stations["kthree"] = set(["or", "nv", "ca"])
+stations["kfour"] = set(["nv", "ut"])
+stations["kfive"] = set(["ca", "az"])
+
+# 最终选择的广播台
+final_stations = set()
+
+while states_needed:
+    best_station = None
+    states_covered = set()
+    for station, states in stations.items():
+        covered = states_needed & states
+        if len(covered) > len(states_covered):
+            best_station = station
+            states_covered = covered
+
+    states_needed -= states_covered
+    final_stations.add(best_station)
+
+print(final_stations)
